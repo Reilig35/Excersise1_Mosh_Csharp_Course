f8c03189412133c754debb57feb0c08dcc1c0a34
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-08-21 12:52:12 +0100

    Add project files.

---
+++ Excersise_Mosh/Methods.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Excersise_Mosh
+{
+    public class Methods
+    {
+        public static void oneToTen(int userInput)
+        {
+            bool userChoice = false;
+            //Problem 1 Start
+            while (userChoice == false)
+            {
+
+                if (userInput >= 1 && userInput <= 10)
+                {
+                    Console.WriteLine($"Your number is {userInput}");
+                    userChoice = true;
+                }
+
+                else if (userInput <= 0 || userInput >= 11)
+                {
+                    Console.WriteLine("Please select a number between 1 and 10");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+                    continue;
+                }
+                //Problem 1 End
+            }
+        }
+
+        //Problem 2 Start
+            public static void maxNum(int num1, int num2)
+        {
+            if(num1 > num2)
+            {
+                Console.WriteLine($"{num1} is the maximum of the inputs");
+            }
+            else if( num1 < num2)
+            {
+                Console.WriteLine($"{num2} is the max of the numbers");
+            }
+            else if (num1 == num2)
+            {
+                Console.WriteLine("Both inputs are equal");
+            }
+            else
+            {
+                Console.WriteLine("No numbers found");
+            }
+        }
+        //Problem 2 End
+
+
+        //Problem 3 Start
+
+        public static void speedCamera(int speed, int limit)
+        {
+            int license = 0;
+            
+            if (speed <= limit)
+            {
+                Console.WriteLine("Have a nice day :)");
+            }
+
+            else if (speed > limit && speed <= limit + 4)
+            {
+                license = license + 2;
+                Console.WriteLine($"You are speeding at {speed - limit}km\\h over the limit\nThat's {license} points on your license");
+            }
+            else if (speed >= (limit+5) && speed <= (limit + 9))
+            {
+                license = license + 4;
+                Console.WriteLine($"You are speeding at {speed - limit}km\\h over the limit\nThat's {license} points on your license");
+            }
+            else if (speed >= (limit + 10) && speed <= (limit + 14))
+            {
+                license = license + 6;
+                Console.WriteLine($"You are speeding at {speed - limit}km\\h over the limit\nThat's {license} points on your license");
+            }
+            else if (speed >= (limit + 15) && speed <= (limit + 19))
+            {
+                license = license + 8;
+                Console.WriteLine($"You are speeding at {speed - limit}km\\h over the limit\nThat's {license} points on your license");
+            }
+            else if (speed >= (limit + 20) && speed <= (limit + 24))
+            {
+                license = license + 10;
+                Console.WriteLine($"You are speeding at {speed - limit}km\\h over the limit\nThat's {license} points on your license");
+            }
+            else if (speed >= (limit + 25))
+            {
+                license = license + 12;
+                Console.WriteLine($"You are speeding at {speed - limit}km\\h over the limit" +
+                    $"\nThat's {license} points on your license" +
+                    $"\nWhich is an automatic ban\n" +
+                    $"Your license has been suspended");
+            }
+            //Problem 3 End
+
+        }
+
+    }
+}
+
+       
---
+++ Excersise_Mosh/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Excersise_Mosh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                //problem 1 Start
+                Console.WriteLine("Please enter a number between 1 and 10:\n");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+
+                Methods.oneToTen(userInput);
+                //Problem 1 End
+
+                //Problem 2 Start
+                Console.WriteLine("Please enter any number:\n");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please enter another number:\n");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Methods.maxNum(num1, num2);
+                //Problem 2 End
+
+                //Problem 3 Start
+                Console.WriteLine("Please enter your current speed in km\\h:\n");
+                int num3 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please the speed limit in km\\h:\n");
+                int num4 = Convert.ToInt32(Console.ReadLine());
+                Methods.speedCamera(num3, num4);
+                //Problem 3 End
+        }
+    }
+}
+
+            
